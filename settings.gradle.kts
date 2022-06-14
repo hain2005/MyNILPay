47cee589498a77973eadf87d4f2619578beee35c
Hai Nguyen <hai.nguyen@dialexa.com>
2022-06-14 13:24:22 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyNILPay"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/mynilpay/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.mynilpay
+
+expect class Platform() {
+    val platform: String
+}
